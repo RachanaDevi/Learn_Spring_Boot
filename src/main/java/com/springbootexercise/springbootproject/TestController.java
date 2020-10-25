5e1b4b3ddb2264abe38863e25950ad6992557712
Rachana Devi <rachanadevi18@gmail.com>
2020-10-25 22:27:17 +0530

    [Rachana] Add. test.jsp which is downloadable from / route

--- src/main/java/com/springbootexercise/springbootproject/TestController.java
+++ src/main/java/com/springbootexercise/springbootproject/TestController.java
@@ -2,14 +2,13 @@ package com.springbootexercise.springbootproject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@ResponseBody //because you want to return the string as it is
 public class TestController {
 
-     @RequestMapping("/test")
+     @RequestMapping("/")
     public String firstHandler(){
-        return "Just for testing!";
+         System.out.println("This is home page");
+        return "test";
     }
 }
