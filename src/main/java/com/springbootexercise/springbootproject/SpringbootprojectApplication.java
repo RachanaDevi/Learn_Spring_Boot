26cec9753deb526a2a1d144403c9ed650c8476ec
Rachana Devi <rachanadevi18@gmail.com>
2020-10-27 13:48:11 +0530

    [Rachana] Refactor. remove unnecessary imports

--- src/main/java/com/springbootexercise/springbootproject/SpringbootprojectApplication.java
+++ src/main/java/com/springbootexercise/springbootproject/SpringbootprojectApplication.java
@@ -1,11 +1,7 @@
 package com.springbootexercise.springbootproject;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScans;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class SpringbootprojectApplication {
